71baaeb2990a47978658d7dc590c4779d2ad73cb
atetheone <atevirran@gmail.com>
2024-08-27 09:30:09 +0000

    update: Use JobRepository interface

--- src/main/java/tech/atetheone/jobboard/job/impl/JobServiceImpl.java
+++ src/main/java/tech/atetheone/jobboard/job/impl/JobServiceImpl.java
@@ -2,55 +2,61 @@ package tech.atetheone.jobboard.job.impl;
 
 import org.springframework.stereotype.Service;
 import tech.atetheone.jobboard.job.Job;
+import tech.atetheone.jobboard.job.JobRepository;
 import tech.atetheone.jobboard.job.JobService;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
-  private List<Job> jobs = new ArrayList<>();
+  private JobRepository jobRepository;
 
-  @Override
-  public List<Job> findAll() {
-    return this.jobs;
+  public JobServiceImpl(JobRepository jobRepository) {
+    this.jobRepository = jobRepository;
   }
 
   @Override
-  public Job findById(Long id) {
-    for (Job j : this.jobs) {
-      if (j.getId() == id) {
-        return j;
-      }
-    }
-    return null;
+  public List<Job> findAll() {
+    return jobRepository.findAll();
+  }
+
+  @Override
+  public Optional<Job> findById(Long id) {
+    return jobRepository.findById(id);
   }
 
   @Override
   public void create(Job job) {
-    this.jobs.add(job);
+    jobRepository.save(job);
   }
 
   @Override
-  public void update(Long id, Job job) {
-    for (Job j : this.jobs) {
-      if (j.getId() == id) {
-        j.setTitle(job.getTitle());
-        j.setDescription(job.getDescription());
-        j.setLocation(job.getLocation());
-        j.setMinSalary(job.getMinSalary());
-        j.setMaxSalary(job.getMaxSalary());
-      }
+  public boolean update(Long id, Job job) {
+    Optional<Job> optionalJob = jobRepository.findById(id);
+
+    if (optionalJob.isPresent()) {
+      Job newJob = optionalJob.get();
+      newJob.setTitle(job.getTitle());
+      newJob.setDescription(job.getDescription());
+      newJob.setLocation(job.getLocation());
+      newJob.setMinSalary(job.getMinSalary());
+      newJob.setMaxSalary(job.getMaxSalary());
+      jobRepository.save(newJob);
+      return true;
     }
+
+    return false;
   }
 
   @Override
-  public void delete(Long id) {
-    for (Job j : this.jobs) {
-      if (j.getId() == id) {
-        this.jobs.remove(j);
-        break;
-      }
+  public boolean delete(Long id) {
+    if (jobRepository.existsById(id)) {
+      jobRepository.deleteById(id);
+      return true;
     }
+    return false;
   }
+
 }
