8326e25a8f2160bced3215df6dc7dcc424334df4
atetheone <atevirran@gmail.com>
2024-08-28 22:52:17 +0000

    feature: Define the Company repository

---
+++ src/main/java/tech/atetheone/jobboard/company/CompanyRepository.java
@@ -0,0 +1,6 @@
+package tech.atetheone.jobboard.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
