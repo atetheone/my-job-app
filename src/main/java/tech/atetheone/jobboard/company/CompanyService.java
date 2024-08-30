6f95097a6e69a76a941a8758bf3f05c30ab41599
atetheone <atevirran@gmail.com>
2024-08-30 21:37:58 +0000

    Update: Change findCompanyById return type

--- src/main/java/tech/atetheone/jobboard/company/CompanyService.java
+++ src/main/java/tech/atetheone/jobboard/company/CompanyService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public interface CompanyService {
   List<Company> findAllCompanies();
-  Optional<Company> findCompanyById(Long id);
+  Company findCompanyById(Long id);
   void createCompany(Company company);
   boolean updateCompany(Long id, Company company);
   boolean deleteCompany(Long id);
